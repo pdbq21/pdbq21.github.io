e7d6770b3c0a29060bbca863f1246a46a2df5dfd
ruslan <pdbq2122@gmail.com>
2016-11-19 23:00:38 +0200

    init: react

---
+++ coming_soon.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+
+
+    render(){
+
+        return(<div>
+              <p>react component</p>
+          </div>);
+    }
+}
+
+
+
+ReactDOM.render(
+  <App />, document.getElementById('root-react')
+);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+
+
+    entry: "./coming_soon.js",
+    output: {
+        filename: "build.js"
+    },
+
+    watch: true,
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == "development" ? "cheap-module-inline-source-map" : null,
+
+    plugins: [
+        /*new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true
+            }
+        })*/
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: ['babel'],
+                /* include: [
+                 path.resolve(__dirname, "src"),
+                 ],*/
+                exclude: /node_modules/,
+                query: {
+                    plugins: ['transform-runtime']
+                }
+            }
+
+        ]
+    }
+
+};
+
+
