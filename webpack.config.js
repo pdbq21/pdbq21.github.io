e66ddf401ef196a617a1319557f9d6a89e416d19
ruslan <pdbq2122@gmail.com>
2016-11-20 13:49:54 +0200

    merge dev_laptop

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
-/**
- * Created by ruslan on 29.08.16.
- */
-var path = require('path');
-var webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        'babel-polyfill',
-        './src/index'
-    ],
+
+
+    entry: "./coming_soon/coming_soon.js",
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+        filename: "build.js"
     },
+
+    watch: true,
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == "development" ? "cheap-module-inline-source-map" : null,
+
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        /*new webpack.optimize.UglifyJsPlugin({
+         compress: {
+         warnings: false,
+         drop_console: true
+         }
+         })*/
     ],
+
     module: {
-        preLoaders: [ //добавили ESlint в preloaders
-            {
-                test: /\.js$/,
-                loaders: ['eslint'],
-                include: [
-                    path.resolve(__dirname, "src"),
-                ],
-            }
-        ],
         loaders: [
             {
-                loaders: ['react-hot', 'babel-loader'], //добавили loader 'react-hot'
-                include: [
-                    path.resolve(__dirname, "src"),
-                ],
-                test: /\.js$/,
-                plugins: ['transform-runtime'],
+                test: /\.jsx?$/,
+                loader: ['babel'],
+                /* include: [
+                 path.resolve(__dirname, "src"),
+                 ],*/
+                exclude: /node_modules/,
+                query: {
+                    plugins: ['transform-runtime']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
             }
+
         ]
     }
+
 };
+
+
